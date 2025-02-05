e972641437b34f5ffde3bd7bbbf19e389b818a6d
Dmitry <dmitrydalidovich@gmail.com>
2025-02-05 13:45:57 +0300

    Initial commit

---
+++ src/main/java/by/koronatech/officeStaffMgmt/OfficeStaffMgmtApplication.java
@@ -0,0 +1,13 @@
+package by.koronatech.officeStaffMgmt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OfficeStaffMgmtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OfficeStaffMgmtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/by/koronatech/officeStaffMgmt/OfficeStaffMgmtApplicationTests.java
@@ -0,0 +1,13 @@
+package by.koronatech.officeStaffMgmt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OfficeStaffMgmtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
